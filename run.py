b82a6c416dfbaf8e867b06bb4fe917988ff2ae97
Lukasz Rajewski <lukasz.rajewski@orange.com>
2020-06-16 16:10:42 +0200

    Variations of model parameters
    - Demands for actions
    - variated service demands
    - Fixed allocation of demands
    - Parametrization of demand shape
    - One-Service allocation mode

--- run.py
+++ run.py
@@ -53,33 +53,85 @@ def round_sig(number, sig):
         return value
 
 
-def get_initial_action_demand_load(iteration, initial_function_placement, total_action_counter, action_slots_number,
-                                   action_demand_level):
+def get_initial_action_information(iteration, joint_actions_allocation, initial_function_placement,
+                                   total_action_counter, action_slots_number, action_demand_level,
+                                   change_action_demands):
+    action_demand = [[[[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]],
+                              [[[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
     initial_action_counter = [[[[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]],
                               [[[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
-    print("Action Schedule")
-    new_demand = [[0, 0], [0, 0]]
-    random.seed(iteration)
-    for s in range(2):
-        for f in range(2):
-            required_demand = action_demand_level * (f + 1) # twice the amount for DNS
-            if iteration > 1:
-                diff = required_demand - math.floor(total_action_counter[s][f] / action_slots_number)
-                required_demand += diff
-            print("ACT Demand[{},{}]) = {}".format(s, f, required_demand))
-            while new_demand[s][f] < required_demand:
-                l = random.randint(0, 4)
-                a = 1
-                if initial_action_counter[s][l][f][a] < initial_function_placement[s][l][f]:
-                    initial_action_counter[s][l][f][a] += 1
-                    new_demand[s][f] += 1
 
-    print("Total Demand: [[{},{}], [{},{}]]".format(new_demand[0][0], new_demand[0][1], new_demand[1][0], new_demand[1][1]))
+    result = {
+        "actionDemands": action_demand,
+        "initialActionCounter": initial_action_counter
+    }
 
-    return initial_action_counter
+    max_break_counter = 100
+
+    if joint_actions_allocation:
+        print("Action Demands")
+        new_demand = [[0, 0], [0, 0]]
+        random.seed(iteration)
+        for s in range(2):
+            for f in range(2):
+                required_demand = action_demand_level * (f + 1) * action_slots_number  # twice the amount for DNS
+                if iteration > 1:
+                    diff = required_demand - total_action_counter[s][f]
+                    required_demand += diff
+                print("ACT Demand[{},{}]) = {}".format(s, f, required_demand))
+                counter = 0
+                while new_demand[s][f] < required_demand:
+                    counter += 1
+                    if counter > max_break_counter:
+                        raise Exception("Cannot allocate requested action demand {}".format(required_demand))
+                    l = random.randint(0, 4)
+                    a = 1
+                    if action_demand[s][l][f][a] < 100000:  # (initial_function_placement[s][l][f] * action_slots_number):
+                        action_demand[s][l][f][a] += 1
+                        new_demand[s][f] += 1
+                        counter = 0
+
+        print("Total Demand: [[{},{}], [{},{}]]".format(new_demand[0][0], new_demand[0][1], new_demand[1][0], new_demand[1][1]))
+
+    else:
+        print("Action Schedule")
+        new_demand = [[0, 0], [0, 0]]
+        random.seed(iteration)
+        for s in range(2):
+            for f in range(2):
+                required_demand = action_demand_level * (f + 1)  # twice the amount for DNS
+                if iteration > 1:
+                    diff = required_demand - math.floor(total_action_counter[s][f] / action_slots_number)
+                    required_demand += diff
+                print("ACT Demand[{},{}]) = {}".format(s, f, required_demand))
+                counter = 0
+                while new_demand[s][f] < required_demand:
+                    counter += 1
+                    if counter > max_break_counter:
+                        raise Exception("Cannot allocate requested action demand {}".format(required_demand))
+                    l = random.randint(0, 4)
+                    a = 1
+                    if initial_action_counter[s][l][f][a] < initial_function_placement[s][l][f]:
+                        initial_action_counter[s][l][f][a] += 1
+                        new_demand[s][f] += 1
+                        counter = 0
+
+        print("Total Demand: [[{},{}], [{},{}]]".format(new_demand[0][0] * action_slots_number,
+                                                        new_demand[0][1] * action_slots_number,
+                                                        new_demand[1][0] * action_slots_number,
+                                                        new_demand[1][1] * action_slots_number))
+
+    return result
 
 
-async def run_allocation(slot_multiplier, max_iterations, action_demand_level, joint_actions):
+def gen_traffic_demand_information(iteration, initial_function_placement, traffic_demand_level, change_traffic_demands):
+    traffic_demands = [[40, 15, 20, 5, 25], [15, 20, 30, 15, 20]]
+
+    return traffic_demands
+
+
+async def run_allocation(slot_multiplier, max_iterations, demand_level, joint_actions_allocation,
+                         change_traffic_demands, change_action_demands):
     # Transform Model into a instance
     coin_bc = minizinc.Solver.lookup("coin-bc")
     # coin_bc = minizinc.Solver.load(Path("./config.msc"))
@@ -92,12 +144,17 @@ async def run_allocation(slot_multiplier, max_iterations, action_demand_level, j
     orchestration_model = minizinc.Model("./orchestration_model.mzn")
     orchestration_model.add_file("./model_data.dzn", False)
     # print(coin_bc.output_configuration())
+
     target_placement = minizinc.Instance(coin_bc, placement_model)
     target_placement["NUM_TIME_SLOTS"] = 1
+    target_placement["ITERATION_NO"] = 1
     initial_demand_allocation = [[[16, 0, 0, 0, 0], [0, 15, 0, 0, 0], [0, 1, 16, 2, 0], [0, 0, 0, 5, 0],[0, 0, 0, 4, 16]],
                                  [[15, 0, 0, 0, 0], [1, 16, 0, 0, 0], [0, 0, 16, 0, 0], [0, 0, 0, 15, 0], [0, 0, 0, 1, 16]]]
     initial_function_placement = [[[4, 8], [4, 8], [4, 8], [4, 8], [4, 8]], [[4, 8], [4, 8], [4, 8], [4, 8], [4, 8]]]
     remaining_time_slots = [[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]], [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]]
+    service_demands = gen_traffic_demand_information(1, initial_function_placement, demand_level,
+                                                     change_traffic_demands)
+    target_placement["serviceDemands"] = service_demands
     target_placement["initialDemandAllocation"] = initial_demand_allocation
     target_placement["initialFunctionPlacement"] = initial_function_placement
     target_placement["extraTimeSlots"] = remaining_time_slots
@@ -120,7 +177,8 @@ async def run_allocation(slot_multiplier, max_iterations, action_demand_level, j
     targetDemandAllocationCost = final_placement_solution.targetDemandAllocationCost
     maxActionDuration = final_placement_solution.maxActionDuration
 
-    num_time_slots = maxActionDuration * slot_multiplier + 1
+    action_time_slots = maxActionDuration * slot_multiplier
+    num_time_slots = action_time_slots + 1
     final_objective = 1
     iterations = 0
     avgSla = [0, 0]
@@ -132,9 +190,10 @@ async def run_allocation(slot_multiplier, max_iterations, action_demand_level, j
     while iterations <= max_iterations and final_objective > roundPlacementSolution:
         start_time = time.time()
         iterations += 1
-        print("{} Calculation for {} time slots".format(iterations, num_time_slots - 1))
+        print("{} Calculation for {} time slots".format(iterations, action_time_slots))
         orchestration_allocation = minizinc.Instance(coin_bc, orchestration_model)
         orchestration_allocation["NUM_TIME_SLOTS"] = num_time_slots
+        orchestration_allocation["ITERATION_NO"] = iterations
         orchestration_allocation["targetDemandAllocations"] = targetDemandAllocations
         orchestration_allocation["functionPlacementTarget"] = functionPlacementTarget
         orchestration_allocation["targetSlaSatisfaction"] = targetSlaSatisfaction
@@ -142,10 +201,14 @@ async def run_allocation(slot_multiplier, max_iterations, action_demand_level, j
         orchestration_allocation["targetDemandAllocationCost"] = targetDemandAllocationCost
         orchestration_allocation["initialDemandAllocation"] = initial_demand_allocation
         orchestration_allocation["initialFunctionPlacement"] = initial_function_placement
-        initial_action_counter = get_initial_action_demand_load(iterations, initial_function_placement,
-                                                                lastActionCounter, num_time_slots - 1,
-                                                                action_demand_level)
-        orchestration_allocation["initialActionCounter"] = initial_action_counter
+        service_demands = gen_traffic_demand_information(iterations, initial_function_placement, demand_level,
+                                                         change_traffic_demands)
+        orchestration_allocation["serviceDemands"] = service_demands
+        action_allocation = get_initial_action_information(iterations, joint_actions_allocation,
+                                                           initial_function_placement, lastActionCounter,
+                                                           action_time_slots, demand_level, change_action_demands)
+        orchestration_allocation["initialActionCounter"] = action_allocation["initialActionCounter"]
+        orchestration_allocation["actionDemands"] = action_allocation["actionDemands"]
         orchestration_allocation["extraTimeSlots"] = remaining_time_slots
         # Solve the allocation problem
         final_allocation_solution = None
@@ -184,4 +247,4 @@ async def run_allocation(slot_multiplier, max_iterations, action_demand_level, j
 # print(get_last_slot([[[2, 3], [1, 6], [0, 4]], [[-2, 5], [0, -3], [2, 10]]], 3))
 # print(round_sig(-3454324234, 20))
 
-asyncio.run(run_allocation(2, 5, 15, False))
+asyncio.run(run_allocation(3, 4, 15, False, False, False))
