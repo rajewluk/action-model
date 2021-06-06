b3e8523b322ebaa4398fd43820238fe9dd6daeeb
rajewluk <lukasz.rajewski@orange.com>
2021-06-06 21:20:44 +0200

    Changed run_all

--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-nohup python3 run.py plan-rnd-fix-no_cl.cfg &
-nohup python3 run.py plan-rnd-fix-cl.cfg &
-nohup python3 run.py plan-rnd-fix-joint.cfg &
-
-nohup python3 run.py plan-rnd-rnd-no_cl.cfg &
-nohup python3 run.py plan-rnd-rnd-cl.cfg &
-nohup python3 run.py plan-rnd-rnd-joint.cfg &
-
 nohup python3 run.py plan-fix-fix-no_cl.cfg &
 nohup python3 run.py plan-fix-fix-cl.cfg &
 nohup python3 run.py plan-fix-fix-joint.cfg &
